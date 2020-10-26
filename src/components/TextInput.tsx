4320db097f0bea1e27e7a38a8cbdfdadc102bd2e
rui-watanabe <wataru610806@gmail.com>
2020-10-26 16:09:31 +0900

    create RadioInput.tsx component

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 interface OwnProps {
-  title: string
-  inputValue: string
-  onChangeValue: Function
+    title: string
+    inputValue: string
+    onChangeValue: Function
 }
 
 const TextInput: React.FC<OwnProps> = (props: OwnProps) => {
-  return(
-    <>
-      <span>{props.title}</span>
-      <input name={props.title} type="text" value={props.inputValue} onChange={e => props.onChangeValue(e.target.value)}></input>
-    </>
-  )
+    return(
+        <div>
+            <span>{props.title}</span>
+            <input name={props.title} type="text" value={props.inputValue} onChange={e => props.onChangeValue(e.target.value)}></input>
+        </div>
+    )
 }
 
 export default TextInput;
